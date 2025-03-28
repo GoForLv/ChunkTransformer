d6e27d72a6d39b54aa69e6213912af0c1292b236
root <zww>
2025-03-28 11:58:00 +0000

    3.28实验完成

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 # Debug
-python train.py --model=TorchTransformer --seq_len=16 --epochs=2 --train_count=1 --debug
-python train.py --model=MultiHeadTransformer --seq_len=16 --epochs=2 --train_count=1 --debug
-python train.py --model=MaskTransformer --seq_len=16 --epochs=2 --train_count=1 --debug
-python train.py --model=ChunkTransformer --seq_len=16 --epochs=2 --train_count=1 --debug
+python train.py --model=TorchTransformer --seq_len=1024 --epochs=3 --train_count=1 --debug
+python train.py --model=MultiHeadTransformer --seq_len=1024 --epochs=3 --train_count=1 --debug
+python train.py --model=MaskTransformer --seq_len=1024 --epochs=3 --train_count=1 --debug
+python train.py --model=ChunkTransformer --seq_len=1024 --epochs=3 --train_count=1 --debug
 
 # 3.27 阿里云实验脚本
 # python train.py --model=MultiHeadTransformer --seq_len=256 --epochs=50 --train_count=3
