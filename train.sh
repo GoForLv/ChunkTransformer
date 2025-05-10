0ecef594a28aaeee076f2e47af49912e20ab0931
zww <2831335581@qq.com>
2025-05-10 22:18:19 +0800

    5_10 train

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ python train.py --data=ETTh1 --model=Torch --epochs=5 --batch_size=64 --seq_len=
 python train.py --data=ETTh1 --model=Base --epochs=5 --batch_size=64 --seq_len=128
 python train.py --data=ETTh1 --model=LocalHBA --epochs=5 --batch_size=64 --d_block=8 --seq_len=128
 python train.py --data=ETTh1 --model=HBA --epochs=5 --batch_size=64 --d_block=8 --seq_len=128
-python train.py --data=ETTh1 --model=Linformer --epochs=5 --batch_size=64 --d_block=8 --seq_len=128
+python train.py --data=ETTh1 --model=Linformer --epochs=5 --batch_size=64 --seq_len=128
